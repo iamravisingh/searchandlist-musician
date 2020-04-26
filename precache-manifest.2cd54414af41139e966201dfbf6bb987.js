ab0a6484b652e8f28d85ac69c697ba43bb9cdac6
Ravi Singh <ravi.singh@adapty.com>
2020-04-27 01:29:07 +0530

    Updates

---
+++ precache-manifest.2cd54414af41139e966201dfbf6bb987.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24ce8550c74ae671a7b6cc316b555767",
+    "url": "/searchandlist-musician/index.html"
+  },
+  {
+    "revision": "ee1989afa978ed80d7cb",
+    "url": "/searchandlist-musician/static/css/main.06ad17be.chunk.css"
+  },
+  {
+    "revision": "77193bd1c89a5fd94514",
+    "url": "/searchandlist-musician/static/js/2.d5dfebf4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/searchandlist-musician/static/js/2.d5dfebf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee1989afa978ed80d7cb",
+    "url": "/searchandlist-musician/static/js/main.18629838.chunk.js"
+  },
+  {
+    "revision": "ec2ca1794e55bedb5eed",
+    "url": "/searchandlist-musician/static/js/runtime-main.bb1a8aeb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/searchandlist-musician/static/media/logo.5d5d9eef.svg"
+  }
+]);
